d05be193e69dcf4a8915c1e1824d33e0cb1f1b15
Ryan Gabriel Souza Martins <ryangabriel_cap@hotmail.com>
2025-01-22 23:24:35 +0000

    feat: create MyLogo component

---
+++ src/components/my-logo.tsx
@@ -0,0 +1,9 @@
+export const MyLogo = () => {
+  return(
+    <div className="text-2xl font-semibold text-foreground">
+        <span className="tracking-wide">RX</span>
+        <span className="text-sm">DEV</span>
+        <span className="text-xs align-end text-yellow-500">.js</span>
+    </div>
+  )
+}
