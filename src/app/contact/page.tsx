406ca5dfac5ca6c26e18cd3ea112d020116532bc
Ryan Gabriel Souza Martins <ryangabriel_cap@hotmail.com>
2025-01-22 23:26:31 +0000

    feat: added app pages and default layout

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,22 @@
+import { Input } from "@/components/input";
+import { Label } from "@/components/label";
+
+
+export default function Contact() {
+  return (
+    <div className="max-w-xl m-auto">
+      <Label>
+        Email
+        <Input placeholder="Digite o seu email"/>
+      </Label>
+      <Label>
+        Email
+        <Input placeholder="Digite o seu email"/>
+      </Label>
+      <Label>
+        Email
+        <Input placeholder="Digite o seu email"/>
+      </Label>
+    </div>
+  );
+}
